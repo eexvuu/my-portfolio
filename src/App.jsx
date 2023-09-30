c743eca199ec29f8c17e8cd7e5fdd5932591f2e3
usamah abdul matin <eexvuu@gmail.com>
2023-09-30 11:29:48 +0700

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import "./App.css";
+import React, { Component } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Experience from "./pages/Experience";
+import Skills from "./pages/Skills";
+import Interest from "./pages/Interest";
+import Awards from "./pages/Awards";
 
-function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/interest" element={<Interest />} />
+          <Route path="/awards" element={<Awards />} />
+        </Routes>
+      </>
+    );
+  }
 }
 
 export default App;
